3bffc062749067efbba59cb02b82a6145ea8d221
Yura Krohmal <yury.krohmal@ardas.dp.ua>
2017-11-15 16:08:49 +0200

    Prepare release 1.2.15

--- Headers/AllUniteSdk.h
+++ Headers/AllUniteSdk.h
@@ -22,21 +22,21 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    BOOL AllUnite_isSdkEnabled();
+    BOOL AllUnite_isSdkEnabled(void);
     
     int  AllUnite_InitSDK(const char *accountId, const char *accountKey);
     void AllUnite_Track(const char *actionCategory, const char *actionId);
     
-    BOOL AllUnite_isBeaconTracking();
-    int AllUnite_SendBeaconTrack();
-    void AllUnite_StopBeaconTrack();
+    BOOL AllUnite_isBeaconTracking(void);
+    int AllUnite_SendBeaconTrack(void);
+    void AllUnite_StopBeaconTrack(void);
     
-    BOOL AllUnite_isDeviceBounded();
-    void AllUnite_BindDevice();
+    BOOL AllUnite_isDeviceBounded(void);
+    void AllUnite_BindDevice(void);
     void AllUnite_BindFbProfile(const char *profileToken, const char *profileData);
     
-    BOOL AllUnite_isLocationAvailable();
-    void AllUnite_RequestAlwaysAuthorization();
+    BOOL AllUnite_isLocationAvailable(void);
+    void AllUnite_RequestAlwaysAuthorization(void);
 #ifdef __cplusplus
 }
 #endif
